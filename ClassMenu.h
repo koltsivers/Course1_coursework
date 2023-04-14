7d4caa012557957d733048f947936115e1789397
Daniil Atavin <93320124+Beescute@users.noreply.github.com>
2023-04-14 18:15:41 +0300

    Update ClassMenu.h

--- ClassMenu.h
+++ ClassMenu.h
@@ -1,3 +1,70 @@
 #pragma once
-#include <string>
+#include <string.h>
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+
+class ClassMenuEdit {
+private:
+	int key;
+public:
+	int getKey(int key) {
+		this->key = key;
+	}
+	void checkKeyGetch(int _count) {
+		//стрелка вверх
+		int count = _count;
+		if (key == 37) {
+			for (int i = 0; i < count;i++) {
+
+			}
+		}
+		//стрелка вправо
+		if (key == 37) {
+
+		}
+		if (key == 13) {
+
+		}
+	}
+};
+
+class ClassMenu
+{
+private:
+	std::string upTitle;
+	std::string items[100];
+public:
+
+	ClassMenu(std::string upTitle) {
+		this->upTitle = upTitle;
+	}
+	int getItemCount() {
+		int count = 0;
+		for (int i = 0; i < 100; i++) {
+			if (items[i].length() > 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+	void addItemMenu(std::string _item) {
+		int count = getItemCount();
+		items[count] = _item;
+	}
+	void deleteItemMenu(std::string _item) {
+		int count = getItemCount();
+		items[count] = "";
+	}
+	void drawMenu() {
+		system("cls");
+		HANDLE hOUTPUT = GetStdHandle(STD_OUTPUT_HANDLE);
+		std::cout << upTitle << std::endl;
+		int count = getItemCount();
+		for (int i = 0; i < count; i++) {
+			std::cout << i << "."  << items[i] << std::endl;
+		}
+	}
+};
