da3746cf39fd8e7dce02d1e1d9b5d30c7122bb2e
Daniil Atavin <93320124+Beescute@users.noreply.github.com>
2023-05-14 11:06:13 +0300

    fix

--- ClassStudent.h
+++ ClassStudent.h
@@ -2,221 +2,245 @@
 #include <string.h>
 #include <string>
 #include <iostream>
-#include "Exams.h"
+#include "ClassExams.h"
 using namespace std;
-class clStudent {
+class ClassStudent {
 private:
-	string name = "";
-	string surname = "";
-	string patronymic = "";
-	int birthDay;
-	int birthMonth;
-	int birthYear;
-	int startStudyYear;
-	string faculty = "";
-	string department = "";
-	string group = "";
-	string ID = "";
+	char name[30];
+	char surname[30];
+	char patronymic[30];
+	unsigned short birthDay;
+	unsigned short birthMonth;
+	unsigned short birthYear;
+	unsigned short yearStart;
+	char institute[30];
+	char department[30];
+	char group[30];
+	char ID[30];
 	bool sex = false;
-	clExam exam;
+	ClassExam exam;
+	float averageGrade;
 public:
-	clStudent() {
-		string name = "Name";
-		string surname = "Surname";
-		string patronymic = "Patronymic";
-		unsigned short birthDay = 0;
-		unsigned short birthMonth = 0;
-		unsigned short birthYear = 0;
-		unsigned short startStudyYear = 0;
-		string faculty = "Faculty";
-		string department = "Department";
-		string group = "Group";
-		string ID = "ID";
-		bool sex = false;
-		clExam exam;
+	ClassStudent() {
+		strcpy_s(this->name, "Имя");
+		strcpy_s(this->surname, "Фамилия");
+		strcpy_s(this->patronymic, "Отчество");
+		this->birthDay = 0;
+		this->birthMonth = 0;
+		this->birthYear = 0;
+		this->yearStart = 0;
+		strcpy_s(this->institute, "Институт");
+		strcpy_s(this->department, "Кафедра");
+		strcpy_s(this->group, "Группа");
+		strcpy_s(this->ID, "ID");
+		sex = false;
 	};
-	bool checkString(string _str) {
-		string Digits = "0123456789";
-		string str = _str;
-		for (int i = 0; i < (int)(str.size()); i++) {
-			for (int j = 0; j < (int)(Digits.size()); j++) {
-				if (str[i] == Digits[j]) {
-					cout << "Имя/фамилия/отчество не должны содержать цифр" << endl;
-					return false;
-				}
-				else {
-					return true;
-				}
-			}
-		}
-	}
-	bool checkGroup(string _group) {
-		string group = _group;
-		string Digits = "0123456789";
-		string AlphabetRU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-		string ourGroup = "БИСО-01-22";
-		for (int i = 0; i < (int)(ourGroup.size()); i++) {
-			for (int j = 0; j < (int)(_group.size()); j++) {
-				if (_group[j] == ourGroup[i]) {
-					cout << "Наш человек!" << endl;
-				}
-			}
-		}
-		if (group.length() != 10) {
-			cout << "Неверная длина записи группы" << endl;
-			return false;
-		}
-		for (int i = 0; i < (int)(group.size()); i++) {
-			for (int j = 0; j < (int)(AlphabetRU.size()); j++) {
-				while (i < 4) {
-					if (!(group[i] == AlphabetRU[j])) {
-						cout << "Неверная длина записи" << endl;
-						return false;
-					}
-				}
-				if (!((i == 4) && (i == 7) && (group[i] == (char)("-")))) {
-					cout << "Неверная запись группы" << endl;
-					return false;
-				}
-				else {
-					for (int k = 0; k < (int)(Digits.size()); k++) {
-						if (!((i == 5) && (i == 6) && (i == 8) && (i == 9) && (group[i] = Digits[k]))) {
-							cout << "Неверная запись группы" << endl;
-							return false;
-						}
-						else {
-							return true;
-						}
-					}
-				}
-			}
-		}
-	}
-	bool checkID(string _id) {
-		string Digits = "0123456789";
-		string AlphabetRU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-		string id = _id;
-		if (id.length() != 7) {
-			cout << "Неверная длина записи номера" << endl;
-			return false;
-		}
-		for (int i = 0; i < (int)(id.size()); i++) {
-			for (int j = 0; j < (int)(AlphabetRU.size()); j++) {
-				if (id[i] == Digits[j]) {
-					if (!((i == 3) && (id[i] == AlphabetRU[j]))) {
-						cout << "Неверная запись номера" << endl;
-						return false;
-					}
-					else {
-						return true;
-					}
-				}
-				else {
-					cout << "Неверная запись номера" << endl;
-					return false;
-				}
-			}
-		}
-	}
-	bool checkDate(const unsigned short birthDay, const unsigned short birthMonth, const unsigned short birthYear) {
-		if ((birthYear % 4 == 0) && (birthYear % 100 == 0) && (birthYear % 400 == 0)) {
-			if (birthMonth == 2){
-				if (birthDay == 29) {
-					return true;
-				}
-				else {
-					return false;
-				}
-			}
-			else {
-				return false;
-			}
-		}
-		else {
-			if (1 <= birthMonth <= 12) {
-				if ((birthMonth == 1) || (birthMonth == 3) || (birthMonth == 5) || \
-					(birthMonth == 7) || (birthMonth == 8) || (birthMonth == 10) ||
-					(birthMonth == 12)) {
-					if (1 <= birthDay <= 31) {
-						return true;
-					}
-					else {
-						return false;
-					}
-				}
-				else {
-					if (1 <= birthDay <= 30) {
-						return true;
-					}
-					else {
-						return false;
-					}
-				}
-			}
-			else {
-				return false;
-			}
-		}
+
+	ClassStudent(char ID[10]) {
+		strcpy_s(this->name, "Имя");
+		strcpy_s(this->surname, "Фамилия");
+		strcpy_s(this->patronymic, "Отчество");
+		this->birthDay = 0;
+		this->birthMonth = 0;
+		this->birthYear = 0;
+		this->yearStart = 0;
+		strcpy_s(this->institute, "Институт");
+		strcpy_s(this->department, "Кафедра");
+		strcpy_s(this->group, "Группа");
+		strcpy_s(this->ID, "ID");
+		sex = false;
+	};
+	//13 элементов
+	ClassStudent(const char* name, const char* surname, const char* patronymic,\
+		const unsigned short birthDay, const unsigned short birthMonth, const unsigned short birthYear, \
+		const unsigned short yearStart, const char* institute, const char* department, \
+		const char* group,  const char* ID, const bool sex, const ClassExam& exam) {
+		strcpy_s(this->name, name);
+		strcpy_s(this->surname, surname);
+		strcpy_s(this->patronymic, patronymic);
+		this->birthDay = birthDay;
+		this->birthMonth = birthMonth;
+		this->birthYear = birthYear;
+		this->yearStart = yearStart;
+		strcpy_s(this->institute, institute);
+		strcpy_s(this->department, department);
+		strcpy_s(this->group, group);
+		strcpy_s(this->ID, ID);
+		this->sex = sex;
+		this->exam = exam;
+	};
+	//12 элементов
+	ClassStudent(const char* name, const char* surname, const char* patronymic, \
+		const unsigned short birthDay, const unsigned short birthMonth, const unsigned short birthYear, \
+		const unsigned short yearStart, const char* institute, const char* department, \
+		const char* group, const char* ID, const bool sex) {
+		strcpy_s(this->name, name);
+		strcpy_s(this->surname, surname);
+		strcpy_s(this->patronymic, patronymic);
+		this->birthDay = birthDay;
+		this->birthMonth = birthMonth;
+		this->birthYear = birthYear;
+		this->yearStart = yearStart;
+		strcpy_s(this->institute, institute);
+		strcpy_s(this->department, department);
+		strcpy_s(this->group, group);
+		strcpy_s(this->ID, ID);
+		this->sex = sex;
+		this->exam = exam;
+	};
+
+	ClassStudent(const ClassStudent& student) {
+		strcpy_s(this->name, student.name);
+		strcpy_s(this->surname, student.surname);
+		strcpy_s(this->patronymic, student.patronymic);
+		this->birthDay = student.birthDay;
+		this->birthMonth = student.birthMonth;
+		this->birthYear = student.birthYear;
+		this->yearStart = student.yearStart;
+		strcpy_s(this->institute, student.institute);
+		strcpy_s(this->department, student.department);
+		strcpy_s(this->group, student.group);
+		strcpy_s(this->ID, student.ID);
+		this->sex = student.sex;
+		this->exam = student.exam;
 	}
 
-	bool checkSex(const unsigned short _sex) {
-		if ((_sex == 1) || (_sex == 0)) {
+
+	bool operator==(const ClassStudent& student) {
+		return (strcmp(this->ID, student.ID) == 0);
+	}
+
+	bool operator<(const ClassStudent& student)
+	{
+		if (strlen(this->ID) < strlen(student.ID)) 
 			return true;
-		}
-		else {
-			cout << "Введенно неверное значение\n";
+		else if (strlen(this->ID) > strlen(student.ID)) 
 			return false;
-		}
+		if (strcmp(this->ID, student.ID) < 0)
+			return true;
+		return false;
 	}
-	void addName(const string& name) {
-		while (!(checkString(name))) {
-			this->name = name;
-		}
-	}
-	void addSurname(const string& surname) {
-		while (!(checkString(surname))) {
-			this->surname = surname;
-		}
-	}
-	void addPatronymic(const string& patronymic) {
-		while (!(checkString(patronymic))) {
-			this->patronymic = patronymic;
-		}
-	}
-	void addData(const unsigned short birthDay, const unsigned short birthMonth, const unsigned short birthYear) {
-		while (!(checkDate(birthDay, birthMonth, birthYear))) {
-			this->birthDay = birthDay;
-			this->birthMonth = birthMonth;
-			this->birthYear = birthYear;
-		}
-	}
-	void addSStartStudyYear() {
 
+	friend std::ostream& operator<< (std::ostream& out, const ClassStudent& student) {
+		out << student.name << ' ';
+		out << "" << student.surname[0] << '.';
+		out << "" << student.patronymic[0] << '.';
+		out.fill(' ');
+		out.width(10 - strlen(student.name));
+		std::ios::left;
+		out << "" << student.group;
+		return out;
+	};
+
+	float getAverageGrade() {
+		return averageGrade;
 	}
-	void addFaculty(const string& faculty) {
-		while (!(checkString(faculty))) {
-			this->faculty = faculty;
-		}
+
+	char* getName() {
+		return name;
 	}
-	void addDepartment(const string& department) {
-		while (!(checkString(department))) {
-			this->department = department;
-		}
+	char* getSurname() {
+		return surname;
 	}
-	void addGroup(const string& group) {
-		while (!(checkGroup(group))) {
-			this->group = group;
-		}
+	char* getPatronymic() {
+		return patronymic;
 	}
-	void addID(const string& ID) {
-		while (!(checkID(ID))) {
-			this->ID = ID;
-		}
+	unsigned short getBirthDay() {
+		return birthDay;
+	}
+	unsigned short getBirthMonth() {
+		return birthMonth;
+	}
+	unsigned short getBirthYear() {
+		return birthYear;
+	}
+	unsigned short getYearStart() {
+		return yearStart;
+	}
+	char* getInstitute() {
+		return institute;
+	}
+	char* getDepartment() {
+		return department;
+	}
+	char* getGroup() {
+		return group;
+	}
+	char* getID() {
+		return ID;
+	}
+	char getSex() {
+		if (sex == 0) { return 'Ж'; }
+		else { return 'М'; }
+	}
+	ClassExam getExam() {
+		return exam;
+	}
+	void addAverageGrade(float  averageGrade){
+		this->averageGrade = averageGrade;
+	}
+	void addName(const char* name) {
+		strcpy_s(this->name, name);
+	}
+	void addSurname(const char* surname) {
+		strcpy_s(this->surname, surname);
+	}
+	void addPatronymic(const char* patronymic) {
+		strcpy_s(this->patronymic, patronymic);
+	}
+	void addBirth(const unsigned short birthDay, const unsigned short birthMonth, const unsigned short birthYear) {
+		this->birthDay = birthDay;
+		this->birthMonth = birthMonth;
+		this->birthYear = birthYear;
+	}
+	void addYearStart(const unsigned short yearStart) {
+		this->yearStart = yearStart;
+	}
+	void addInstitute(const char* institute) {
+		strcpy_s(this->institute, institute);
+	}
+	void addDepartment(const char* department) {
+		strcpy_s(this->department, department);
+	}
+	void addGroup(const char* group) {
+		strcpy_s(this->group, group);
+	}
+	void addID(const char* ID) {
+		strcpy_s(this->ID, ID);
 	}
 	void addSex(const bool sex) {
-		while (!(checkSex(sex))) {
-			this->sex = sex;
-		}
+		this->sex = sex;
+	}
+
+	int fillExam(const unsigned short _numSess, const char* _nameLesson, \
+		const unsigned short _grade, const unsigned short _numLesson = -1) {
+		return exam.addLesson(_numSess, _nameLesson, _grade, _numLesson);
+	}
+
+	void addExam(const ClassExam& exam) {
+		this->exam = exam;
+	}
+
+	void copyExam(ClassExam& exam) {
+		this->exam = exam;
+	}
+
+	void printDate() {
+		cout << "Дата рождения: " << birthDay << "." << birthMonth << "." << birthYear;
+	}
+
+	void printAllData() {
+		cout << "Имя: " << this->name << endl;
+		cout << "Фамилия: " << this->surname << endl;
+		cout << "Отчество: " << this->patronymic << endl;
+		printDate();
+		cout << "Начало учебы: " << this->yearStart << endl;
+		cout << "Направление: " << this->institute << endl;
+		cout << "Институт: " << this->department << endl;
+		cout << "Номер группы: " << this->group << endl;
+		cout << "Номер студ. билета: " << this->ID<< endl;
+		cout << "Пол: " << this->sex<< endl;
+		exam.print();
 	}
 
 };
