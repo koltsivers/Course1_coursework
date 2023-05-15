942e24d2757b4ffd926bdfbca5aecd620ead4525
Daniil Atavin <93320124+Beescute@users.noreply.github.com>
2023-05-15 11:31:17 +0300

    Update DB_of_students.cpp

--- DB_of_students.cpp
+++ DB_of_students.cpp
@@ -32,7 +32,7 @@ void crypt() {
 	command += pass;
 	system(command.c_str());
 
-	if (remove("file.txt") != 0) {
+	if (remove("dbFile.txt") != 0) {
 		cout << "[ERROR] - deleting file" << endl;
 	}
 
@@ -72,53 +72,74 @@ void decrypt() {
 	system(command.c_str());
 
 
-	if (remove("file.txt.enc") != 0) {
+	if (remove("dbFile.txt.enc") != 0) {
 		cout << "[ERROR] - deleting file" << endl;
 	}
 
 }
 void WRITE(FILE* file) {
-	fopen_s(&file, "file.txt", "w");
+	fopen_s(&file, "dbFile.txt", "w");
+	char firstWord[30] = "Студент номер";
+	char specialSymb[40] = "---------------------------------------";
 	ClassList <ClassStudent> tempStudents;
 	//1
-	ClassStudent Dany("Даниил", "Атавин", "Антонович", 07, 02, 2005, 2022, "IKB2", "KB-02", "БИСО-01-22", "22Б8660", 1);
+	ClassStudent Dany("Даниил", "Атавин", "Антонович", 07, 02, 2005, 2022, "ИКБ", "КБ-02", "БИСО-01-22", "22Б8660", 1 );
+	Dany.fillExam(0, "Физика", 5); Dany.fillExam(0, "История", 5); Dany.fillExam(0, "Линейная алгебра", 5); Dany.fillExam(0, "Математический анализ", 5); Dany.fillExam(0, "Основы ОИБ", 5);
 	tempStudents.addElem(Dany);
 	//2
-	ClassStudent Maxim("Максим", "Бовин", "Олегович", 23, 05, 2003, 2022, "IKB2", "KB-03", "БАСО-03-22", "22Б8661", 1);
+	ClassStudent Maxim("Максим", "Бовин", "Олегович", 23, 05, 2003, 2022, "ИКБ", "КБ-03", "БАСО-03-22", "22Б8661", 1);
+	Maxim.fillExam(0, "Физика", 4); Maxim.fillExam(0, "История", 5); Maxim.fillExam(0, "Линейная алгебра", 4); Maxim.fillExam(0, "Математический анализ", 4);
 	tempStudents.addElem(Maxim);
 	//3
-	ClassStudent Ksenia("Ксения", "Брайнин", "Алексеевна", 30, 10, 2004, 2022, "IKB2", "KB-03", "БАСО-03-22", "22Б1003", 0);
+	ClassStudent Ksenia("Ксения", "Брайнин", "Алексеевна", 30, 10, 2004, 2022, "ИКБ", "КБ-03", "БАСО-03-22", "22Б1003", 0);
+	Ksenia.fillExam(0, "Физика", 3); Ksenia.fillExam(0, "История", 3); Ksenia.fillExam(0, "Линейная алгебра", 4); Ksenia.fillExam(0, "Математический анализ", 3); Ksenia.fillExam(0, "Основы ОИБ", 4);
 	tempStudents.addElem(Ksenia);
 	//4
-	ClassStudent Alina("Алина", "Грибанова", "Владимировна", 01, 02, 2004, 2022, "IKB2", "KB-03", "БАСО-03-22", "22Б0864", 0);
+	ClassStudent Alina("Алина", "Грибанова", "Владимировна", 01, 02, 2004, 2022, "ИКБ", "КБ-03", "БАСО-03-22", "22Б0864", 0);
+	Alina.fillExam(0, "Линейная алгебра", 4); Alina.fillExam(0, "Математический анализ", 4); Alina.fillExam(0, "Основы ОИБ", 4);
 	tempStudents.addElem(Alina);
 	//5
-	ClassStudent Ilya("Илья", "Долгов", "Сергеевич", 15, 03, 2003, 2022, "IKB2", "KB-03", "БАСО-03-22", "22Б0866", 1);
+	ClassStudent Ilya("Илья", "Долгов", "Сергеевич", 15, 03, 2003, 2022, "ИКБ", "КБ-03", "БАСО-03-22", "22Б0866", 1);
+	Ilya.fillExam(0, "Физика", 5); Ilya.fillExam(0, "История", 5); Ilya.fillExam(0, "Линейная алгебра", 5); Ilya.fillExam(0, "Математический анализ", 5); Ilya.fillExam(0, "Основы ОИБ", 5);
 	tempStudents.addElem(Ilya);
 	//6
-	ClassStudent Nastya("Анастасия", "Ерофеева", "Александрова", 06, 06, 2004, 2022, "IKB2", "KB-02", "БАСО-03-22", "22Б0868", 0);
+	ClassStudent Nastya("Анастасия", "Ерофеева", "Александрова", 06, 06, 2004, 2022, "ИКБ", "КБ-02", "БАСО-03-22", "22Б0868", 0);
+	Nastya.fillExam(0, "Физика", 3); Nastya.fillExam(0, "История", 4); Nastya.fillExam(0, "Линейная алгебра", 5); Nastya.fillExam(0, "Математический анализ",5);
 	tempStudents.addElem(Nastya);
 	//7
-	ClassStudent Alex("Александр", "Кондрашкин", "Олегович", 06, 11, 2004, 2022, "IKB2", "KB-02", "БИСО-01-22", "22Б0875", 1);
+	ClassStudent Alex("Александр", "Кондрашкин", "Олегович", 06, 11, 2004, 2022, "ИКБ", "КБ-02", "БИСО-01-22", "22Б0875", 1);
+	Alex.fillExam(0, "Физика", 5); Alex.fillExam(0, "История", 5); Alex.fillExam(0, "Линейная алгебра", 5); Alex.fillExam(0, "Математический анализ", 5); Alex.fillExam(0, "Основы ОИБ", 4);
 	tempStudents.addElem(Alex);
 	//8
-	ClassStudent Vlad("Владислав", "Королёв", "Денисович", 12, 01, 2004, 2022, "IKB2", "KB-02", "БИСО-01-22", "22Б0877", 1);
+	ClassStudent Vlad("Владислав", "Королёв", "Денисович", 12, 01, 2004, 2022, "ИКБ", "КБ-02", "БИСО-01-22", "22Б0877", 1);
+	Vlad.fillExam(0, "Физика", 4); Vlad.fillExam(0, "История", 5); Vlad.fillExam(0, "Линейная алгебра", 4); Vlad.fillExam(0, "Математический анализ", 4); Vlad.fillExam(0, "Основы ОИБ", 4);
 	tempStudents.addElem(Vlad);
 	//9
-	ClassStudent Kostya("Константин", "Папин", "Михайлович", 25, 01, 2005, 2022, "IKB2", "KB-04", "ББББ-06-22", "22Б0884", 1);
+	ClassStudent Kostya("Константин", "Папин", "Михайлович", 25, 01, 2005, 2022, "ИКБ", "КБ-04", "ББББ-06-22", "22Б0884", 1);
+	Kostya.fillExam(0, "Физика", 3); Kostya.fillExam(0, "История", 5); Kostya.fillExam(0, "Математический анализ", 4); Kostya.fillExam(0, "Основы ОИБ", 4);
 	tempStudents.addElem(Kostya);
 	//10
-	ClassStudent Artem("Артем", "Франк", "Александрович", 23, 04, 2004, 2022, "IKB2", "KB-04", "ББББ-06-22", "22Б0896", 1);
+	ClassStudent Artem("Артем", "Франк", "Александрович", 23, 04, 2004, 2022, "ИКБ", "КБ-04", "ББББ-06-22", "22Б0896", 1);
+	Artem.fillExam(0, "Физика", 3); Artem.fillExam(0, "История", 4); Artem.fillExam(0, "Линейная алгебра", 4); Artem.fillExam(0, "Математический анализ", 4); 
 	tempStudents.addElem(Artem);
 
 	for (int i = 0; i < tempStudents.getSize(); i++) {
-		fprintf_s(file, "%s %s %s %d %d %d %d %s %s %s\n", \
+		fprintf_s(file, "%s %d: %s %s %s %d %d %d %d %s %s %s %s\n", firstWord, (i+1), \
 			tempStudents[i].getSurname(), tempStudents[i].getName(), tempStudents[i].getPatronymic(), \
 			tempStudents[i].getBirthDay(), tempStudents[i].getBirthMonth(), tempStudents[i].getBirthYear(), \
 			tempStudents[i].getYearStart(), tempStudents[i].getInstitute(), tempStudents[i].getDepartment(),
 			tempStudents[i].getGroup(), tempStudents[i].getID(), tempStudents[i].getSex());
-	}
 
+		for (int s = 0; s < 9; s++) {
+			for (int l = 0; l < 10; l++) {
+				if (!tempStudents[i].getExam().lessons[s][l].empty) {
+					fprintf_s(file, "Сессия %d, экзамен %d : %s %d\n",  (s+1), (l + 1), \
+						tempStudents[i].getExam().lessons[s][l].nameLesson, tempStudents[i].getExam().lessons[s][l].grade);
+				}
+			}
+		}
+		fprintf_s(file, "%s\n", specialSymb);
+	}
 	fclose(file);
 }
 int main() {
@@ -142,3 +163,4 @@ int main() {
 	mainMenu.draw(file);
 
 };
+
